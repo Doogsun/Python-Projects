81a1e39f6b992a6a6296398417a868267cfa5c57
Daxon Brar <daxbrar@gmail.com>
2025-06-20 22:05:58 -0600

    bleh

--- Day_14_Project.py
+++ Day_14_Project.py
@@ -1,7 +1,7 @@
 """Create a mental math trainer for double digit multiplication (XX * XX). Make modular to add more operations i nthe future"""
 
 def prompting():
-
+    print("Test")
 
 if __name__
 def main():
