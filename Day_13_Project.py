39182586073a3826ed7b96f0a3556b0c35e940b8
Daxon Brar <daxbrar@gmail.com>
2025-06-20 21:58:32 -0600

    Deleted commit error

--- Day_13_Project.py
+++
@@ -1,9 +0,0 @@
-"""Create a mental math trainer for double digit multiplication (XX * XX). Make modular to add more operations i nthe future"""
-
-def prompting():
-
-
-if __name__
-def main():
-    prompting()
-
