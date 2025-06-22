5e089e0188687065b9620f34d0f4f9835edee237
Daxon Brar <daxbrar@gmail.com>
2025-06-22 14:16:18 -0600

    Added range option, started random question generation
    Made a roadmap for projects

--- Day_14_Project.py
+++ Day_14_Project.py
@@ -2,11 +2,10 @@
 
 
 import random
+import time
 
 operation_list = ["+", '-', '*']
-number_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 again = False
-
 
 
 def play_prompt():
@@ -24,17 +23,16 @@ def play_prompt():
             print("Please enter either 'Y' or 'N'")
 
 
-
-
-
 def prompting():
     while True:
         try:
             question_amount = int(input("How many questions would you like to do? (1 - 50): "))
             operation_type = str(input("What operation would you like to do? (+, -, *): "))
+            operation_range = int(input("Select the terms range for the questions (1 - 100): "))
 
-            if 1 <= question_amount <= 50 and operation_type in operation_list:
-                return question_amount, operation_type
+
+            if 1 <= question_amount <= 50 and (operation_type in operation_list) and 1 <= operation_range <= 100:
+                return question_amount, operation_type, operation_range
             else:
                 print("\nPlease enter a question amount between 1 - 50, and a valid operatino symbol.")
 
@@ -42,18 +40,54 @@ def prompting():
             print("\nThe first question requires a int, and the second question needs a str\n")
 
 
-
-
-
+def type_analysis(tupleoutput):
+    if tupleoutput[1] == "+":
+        addition(tupleoutput[0], tupleoutput[2])
+        return None
     
+    elif tupleoutput[1] == "-":
+        subtraction(tupleoutput[0], tupleoutput[2])
+        return None
     
+    elif tupleoutput[1] == "*":
+        multiplication(tupleoutput[0], tupleoutput[2])
+        return None
+
+
+def addition(question_amount, operation_range):
+    correct = 0
+    wrong = 0
+    for question in range (1, question_amount + 1):
+        try:
+            term1 = random.randint(1, operation_range)
+            term2 = random.randint(1, operation_range)
+            answer = int(input(f"{term1} + {term2}: "))
+            
+        except ValueError:
+            print("Please enter a correct answer format (int)")
+           
+ 
+
+
+def subtraction(question_amount, operation_range):
+    print()
+
+def multiplication(question_amount, operation_range):
+    print()
+
 
 
 if __name__ == "__main__":
     again = True
     play1 = play_prompt()
     if play1 == True:
-        prompting()
+       output1 = prompting()
+       type_analysis(output1)
+
+           
+    
+
+
     
         
 
---
+++ Python/Test.py
@@ -0,0 +1,9 @@
+#Project ideas
+#1) For the in terminal project
+#   a slot machine using file /io
+#   a rpg using oop (remake of the third days project)
+#2) For my first pygame project
+#   Dual 2 back
+#   -   audio and positional 
+#   -   n, and trial number
+#   -   non random "randomness"
